1b7875d18c6f59c8a9e368eb4311c9b68a18a680
snakanz <97860316+snakanz@users.noreply.github.com>
2025-07-10 22:09:33 +0100

    Remove unused useNavigate import from Clients.js to fix build error

--- src/pages/Clients.js
+++ src/pages/Clients.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { Box, Typography, Button, Stack, TextField } from '@mui/material';
 import { api } from '../services/api';
 
